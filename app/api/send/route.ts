b5d4e410067f166229f42ffe01a25587a9b58453
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-12-09 06:59:57 +0600

    solve isue

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_RJgHeYzE_7QSCRCpKzXkRbKVmVvDhSRFy');
 
 export async function POST(req: Request) {
   const data = await req.json();
