f82628a48e17a408e3e9a19c36d9fec487dcf5a2
PRONOB BISWAS <biswaspronob@outlook.com>
2025-12-11 10:37:14 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 
 
 import {
-  Header,
+  // Header,
   HeroSection,
-  Process,
-  FAQs,
-  About,
-  CaseStudies,
-  Forecast,
+  // Process,
+  // FAQs,
+  // About,
+  // CaseStudies,
+  // Forecast,
   Footer,
-  Months,
-  DownloadList
+  // Months,
+  // DownloadList
 } from "@/components/index";
 import Beforeourupcomingcall from "@/components/beforeourupcomingcall";
 import Step2 from "@/components/step2";
@@ -18,14 +18,21 @@ import Step2 from "@/components/step2";
 
 
 export default function Home() {
-  return (
-    <div className="relative flex flex-col items-center min-h-screen bg-black w-full overflow-hidden">
-
+  return ( 
+    <div className="w-full min-h-screen relative flex flex-col items-center  bg-black  overflow-hidden">
+             
       {/* <Header /> */}
 
       <HeroSection />
 
-      <main className="main ">
+      <main 
+      style={{
+        backgroundColor:"#001e1b",
+         background: "linear-gradient(rgb(0, 30, 27), #00311840, #1e00314a, #00102d4d, black, black)"
+        
+      }}
+      
+      className="main w-full pt-28 ">
 
 
         {/* <section id="process" className="flex w-full justify-center items-center max-w-5xl mx-auto scroll-mt-[130px] -mt-20">
@@ -62,7 +69,8 @@ export default function Home() {
         <section>
           <Beforeourupcomingcall/>
         </section>
-        <section id="section_step_2" className="w-full h-fit flex justify-center bg-[#00d492]">
+        {/* bg-[#00d492] */}
+        <section id="section_step_2" className="w-full h-fit flex justify-center pt-20 ">
             <Step2 />
         </section>
       </main>
--- components/step2.tsx
+++ components/step2.tsx
@@ -4,15 +4,15 @@ import React from 'react'
 const Step2 = () => {
     return (
         <>
-            <section id='section2' className='w-full h-fit bg-[#00d492] border-8 border-amber-100 ' >
-                <div className='w-full h-fit p-5 hidden'>
+            <section id='section2' className='w-full h-fit' >
+                <div className='w-full h-fit p-5 '>
                     {/* ===heading=== */}
-                    <div className='text-white text-center pb-8'>
+                    <div className='text-white text-center pb-8 '>
                         <span className='text-4xl font-bold text-emerald-400'>STEP 2</span>
                         <h2 className='text-3xl' >Fill out the <span className='text-emerald-400'>form below</span> and <span className='text-emerald-400'>book a call</span></h2>
                     </div>
                     {/* ===forrm section=== */}
-                    <div className='w-full max-w-3xl bg-[#00000059] text-white mx-auto p-5'>
+                    <div className='w-full max-w-3xl bg-[#bb1616ef] text-white mx-auto p-5 hidden'>
                         {/* ---heading--- */}
                         <div>
                             <h2 className='text-3xl font-bold'>Easy Step</h2>
@@ -52,7 +52,8 @@ const Step2 = () => {
 
                     </div>
                 </div>
-                <div className='w-full h-screen flex items-center justify-center'>
+
+                <div className='w-full h-fit min-h-[500px] flex items-center justify-center'>
                     <div className='w-full h-full max-w-6xl p-5'>
                         
                         <iframe
@@ -60,6 +61,11 @@ const Step2 = () => {
                             width="100%"
                             height="100%"
                             title="Typeform"
+                            style={{
+                                width:'100%',
+                                height:'100%',
+                                minHeight:'700px'
+                            }}
                         />
                     </div>
 
